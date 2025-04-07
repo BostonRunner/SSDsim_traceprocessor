cfef9f6434a2ed0c37b4627ba74535d3d47e3148
Boston Runner <69416808+BostonRunner@users.noreply.github.com>
2025-04-07 22:23:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -rf results/
 mkdir results
 cd ./results || exit
 echo "directory created"
@@ -8,9 +8,11 @@ touch result.log
 echo "result.txt result.log created"
 echo "blktrace activating"
 cd ..
+docker kill $(docker ps -q)
 docker system prune -af --volumes
-
+sleep 3
 ./blktrace.sh
+sleep 3
 ./IO.sh
 
 
@@ -20,7 +22,10 @@ sort -n -k 4 result_temp.txt > result.txt
 cd ..
 
 echo "processing I/O trace..." & sleep 60
-
+echo 3 | sudo tee /proc/sys/vm/drop_caches   # 
+# sudo find /mnt/emu -xdev -type f > /dev/null  #
 python3 main.py
+# javac Main.java
+# java Main
 python3 trans.py
 echo "program finished"
