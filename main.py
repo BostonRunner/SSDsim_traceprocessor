53775121ad41dc3d97f227937586e78bdca1e625
Boston Runner <69416808+BostonRunner@users.noreply.github.com>
2025-04-07 22:22:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,95 +1,93 @@
-##################################################################################
-# Python script of handling the output of blkparse and add the path of file      #
-# Please change the directory according to your system and device                #
-# Author: Ruofei Wu                                                              #
-# 09-26-2024                                                                     #
-# Renewed in 09-30-2024                                                          #
-##################################################################################
-
 import os
 import sys
-import time
 
-data_with_path = str()
-io = str()
+# 路径定义
+input_file_path = "./results/result.txt"
+output_file_path = "./results/result_path.txt"
+device_path = "/dev/sda1"
 
-# open the log
-origin_file = open("./results/result.txt", "r")
-final = open("./results/result_path.txt", "w")
-info = os.popen("docker container inspect docker_blktest")
-info = info.readlines()
-LowerDir = str()
-LowerDirList = list()
-UpperDir = str()
+# 获取容器 UpperDir 和 LowerDir 信息
+def get_overlay_paths(container_name):
+    info = os.popen(f"docker container inspect {container_name}").readlines()
+    lower_dirs = []
+    upper_dir = ""
+    for line in info:
+        if "LowerDir" in line:
+            raw = line.split('\"')[-2]
+            if ':' in raw:
+                lower_dirs = [d[11:] for d in raw.split(':') if "init" not in d]
+        elif "UpperDir" in line:
+            upper_dir = line.split('\"')[-2][11:]
+    return upper_dir, lower_dirs
 
-for i in range(len(info)):
-    if "LowerDir" in info[i]:
-        LowerDir = info[i].split('\"')[-2]
-        if ':' in LowerDir:
-            LowerDirList = LowerDir.split(':')
-        LowerDir = LowerDir[11:]
-        print(LowerDirList)
-        time.sleep(2)
-    elif "UpperDir" in info[i]:
-        UpperDir = info[i].split('\"')[-2]
-        UpperDir = UpperDir[11:]
-        print(UpperDir)
-        time.sleep(2)
-    else:
-        pass
+# 收集每个容器的 overlay 路径（编号与列表位置绑定）
+upper_dirs = []
+lower_dirs = []
+for i in range(1, 7):
+    u, l = get_overlay_paths(f"docker_blktest{i}")
+    upper_dirs.append(u)
+    lower_dirs.append(l)  # 形成二维列表，index 对应容器编号 - 1
 
-print("File reading")
-# read the log
-data = origin_file.readlines()
+# 读取 trace 日志
+with open(input_file_path, "r") as infile:
+    trace_lines = infile.readlines()
 
-print("File processing")
-# read the inode
-for i in range(len(data)):
-    block = int(eval(data[i].split()[-1]) / 8)
+result_lines = []
 
-    command = f"debugfs -R 'icheck {block}' /dev/sda1"
-    # print(">> " + command)
-    result_of_icheck = os.popen(command)
-    # print(result_of_icheck)
-    result_of_icheck = result_of_icheck.readlines()[-1]
-    inode_temp = result_of_icheck.split()[-1]
+# 处理每一行 trace
+for i, line in enumerate(trace_lines):
+    try:
+        block_str = line.strip().split()[-1]
+        block = int(eval(block_str) / 8)
 
-    if inode_temp.isdigit():
-        # find the directory
-        inode = int(inode_temp)
-
+        # 使用 debugfs 获取 inode
+        icheck_cmd = f"debugfs -R 'icheck {block}' {device_path}"
+        icheck_result = os.popen(icheck_cmd).readlines()
+        if not icheck_result:
+            continue
+        inode_line = icheck_result[-1]
+        inode_parts = inode_line.strip().split()
+        if not inode_parts or not inode_parts[-1].isdigit():
+            continue
+        inode = int(inode_parts[-1])
         if inode == 8:
-            # data_with_path = data_with_path + '\t' +"system reserved area(inode 8)\n"
-            pass
+            continue  # 跳过系统 inode
 
-        else:
-            command = f"debugfs -R 'ncheck {inode}' /dev/sda1"
-            # print(">>" + command)
-            result_of_ncheck = os.popen(command).readlines()[-1]
-            result_of_ncheck = result_of_ncheck.split()[-1]
-            # print(result_of_ncheck)
-            if UpperDir in result_of_ncheck:
-                data_with_path += data[i][:-1]
-                data_with_path = data_with_path + '\t' + result_of_ncheck + "\t[UpperLayer]" + '\n'
-            else:
-                for j in range(len(LowerDirList)):
-                    if "init" in LowerDirList[j]:
-                        pass
-                    else:
-                        if str(LowerDirList[j])[11:] in result_of_ncheck:
-                            data_with_path += data[i][:-1]
-                            data_with_path = data_with_path + '\t' + result_of_ncheck + "\t[LowerLayer]" + '\n'
+        # 获取路径
+        ncheck_cmd = f"debugfs -R 'ncheck {inode}' {device_path}"
+        ncheck_result = os.popen(ncheck_cmd).readlines()
+        if not ncheck_result:
+            continue
+        path_line = ncheck_result[-1].strip()
+        file_path = path_line.split()[-1]
 
-    else:
-        # data_with_path = data_with_path + '\t' +"directory not found\n"
-        pass
+        # 判断归属容器及层级
+        label = ""
+        container_id = -1
+        for idx in range(6):
+            if file_path.startswith(upper_dirs[idx]):
+                label = "[UpperLayer]"
+                container_id = idx + 1
+                break
+            elif any(ld in file_path for ld in lower_dirs[idx]):
+                label = "[LowerLayer]"
+                container_id = idx + 1
+                break
 
-    print("period: " + str(i) + " / " + str(len(data)))
+        if label and container_id != -1:
+            result_lines.append(f"{line.strip()}\t{file_path}\t{label}\t[Container{container_id}]\n")
+
+    except Exception as e:
+        print(f"\nError at line {i}: {e}")
+        continue
+
+    # 显示进度
+    sys.stdout.write(f"\rProcessing: {i+1}/{len(trace_lines)} ({(i+1)/len(trace_lines)*100:.2f}%)")
     sys.stdout.flush()
 
-# write the result in the new file "result_path.txt"
-print("process finished")
-final.write(data_with_path)
-origin_file.close()
-final.close()
+# 写入结果
+with open(output_file_path, "w") as outfile:
+    outfile.writelines(result_lines)
+
+print("\nProcessing complete! Result written to result_path.txt.")
 
